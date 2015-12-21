Fixed some problems with our rotation stuff

tf_rl/simulation/orbiter_game.py
@@ -121,7 +121,7 @@ class OrbiterGame(object):
 
         unitThrust = np.array([math.cos(self.craft.heading), math.sin(self.craft.heading)])
         pToC = self.craft.position - self.planet.position
-        cToO = np.array([-1*pToC[1], pToC[0]])
+        cToO = np.array([pToC[1], -1*pToC[0]])
         unitCToO = cToO/np.linalg.norm(cToO)
         orbitAngle = np.arccos(unitThrust.dot(cToO)/(np.linalg.norm(cToO)*np.linalg.norm(unitThrust)))
 
@@ -195,7 +195,7 @@ class OrbiterGame(object):
 
         unitThrust = np.array([math.cos(self.craft.heading), math.sin(self.craft.heading)])
         pToC = self.craft.position - self.planet.position
-        cToO = np.array([-1*pToC[1], pToC[0]])
+        cToO = np.array([pToC[1], -1*pToC[0]])
         orbitAngle = np.arccos(unitThrust.dot(cToO)/(np.linalg.norm(cToO)*np.linalg.norm(unitThrust)))
         altitude = (np.linalg.norm(self.craft.position - self.planet.position) - self.planet.radius)
 
@@ -313,7 +313,7 @@ class OrbiterGame(object):
 
         unitThrust = np.array([math.cos(self.craft.heading), math.sin(self.craft.heading)])
         pToC = self.craft.position - self.planet.position
-        cToO = np.array([-1*pToC[1], pToC[0]])
+        cToO = np.array([pToC[1], -1*pToC[0]])
         unitCToO = cToO/np.linalg.norm(cToO)
         orbitAngle = np.arccos(unitThrust.dot(cToO)/(np.linalg.norm(cToO)*np.linalg.norm(unitThrust)))
 
@@ -373,7 +373,7 @@ class OrbiterGame(object):
 
 #        unitThrust = np.array([math.cos(self.craft.heading), math.sin(self.craft.heading)])
 #        pToC = self.craft.position - self.planet.position
-#        cToO = np.array([-1*pToC[1], pToC[0]])
+#        cToO = np.array([pToC[1], -1*pToC[0]])
 #        orbitAngle = np.arccos(unitThrust.dot(cToO)/(np.linalg.norm(cToO)*np.linalg.norm(unitThrust)))
 
         stats = stats[:]